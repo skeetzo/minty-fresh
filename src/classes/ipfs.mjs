2fed0d378f51aa68fefbf8495f088cf16f6d1099
Skeetzo <WebmasterSkeetzo@gmail.com>
2025-04-28 12:16:59 -0700

    saved

--- src/classes/ipfs.mjs
+++ src/classes/ipfs.mjs
@@ -45,6 +45,7 @@ export class IPFS {
 
     // file must have: name, path, and content
     static async add(file, baseUri="ipfs://") {
+        console.log(file)
         console.debug(`adding IPFS path: ${file.path}`);
         const { cid: metadataCID } = await IPFS_CLIENT.add(file, IPFS.ipfsAddOptions);
         const metadataURI = IPFS.ensureIpfsUriPrefix(metadataCID, baseUri) + "/" + file.name;
